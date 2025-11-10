7ed2049fa8dbcda5e1c8c2dfc3a35d7f6c45029d
Maryvi Camacho <macamach@c1r7s4.42porto.com>
2025-11-10 11:34:27 +0000

    funcionando sin counter

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: macamach <mcamach@student.42porto.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 12:17:26 by macamach          #+#    #+#             */
-/*   Updated: 2025/11/10 10:21:29 by macamach         ###   ########.fr       */
+/*   Updated: 2025/11/10 11:33:50 by macamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,11 @@ static int	count_specifiers(const char *format)
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
-	int		i;
-	int		result;
-	char	*base;
+	int				i;
+	int				result;
+	char			*base;
+	char			*str;
+	unsigned long	ul;
 
 	result = 0;
 	if (format == NULL)
@@ -60,27 +62,49 @@ int	ft_printf(const char *format, ...)
 			if (format[i] == '%')
 				ft_putchar_fd('%', 1);
 			if (format[i] == 'd')
-				ft_putnbr_fd(va_arg(args, int), 1);
+			{
+				base = "0123456789";
+				ft_putnbr_base_fd(va_arg(args, int), base, 1);
+			}
 			if (format[i] == 'i')
-				ft_putnbr_fd(va_arg(args, int), 1);
+				base = "0123456789";
+			{
+				base = "0123456789";
+				ft_putnbr_base_fd(va_arg(args, int), base, 1);
+			}
 			if (format[i] == 's')
-				ft_putstr_fd(va_arg(args, char *), 1);
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					ft_putstr_fd("(null)", 1);
+				else
+					ft_putstr_fd(str, 1);
+			}
 			if (format[i] == 'c')
 				ft_putchar_fd(va_arg(args, int), 1);
 			if (format[i] == 'p')
 			{
-				ft_putstr_fd("0x", 1);
-				base = "0123456789abcdef";
-				ft_putnbr_base_fd((unsigned long)va_arg(args, void *), base ,1);
+				ul = (unsigned long)va_arg(args, void *);
+				if (ul == 0)
+					ft_putstr_fd("(nil)", 1);
+				else
+				{
+					ft_putstr_fd("0x", 1);
+					base = "0123456789abcdef";
+					ft_putnbr_base_fd(ul, base ,1);
+				}
 			}
 			if (format[i] == 'u')
-				ft_putnbr_fd((unsigned int)va_arg(args, unsigned int), 1);
+			{
+				base = "0123456789";
+				ft_putnbr_base_fd((unsigned int)va_arg(args, unsigned int), base, 1);
+			}
 			if (format[i] == 'x')
 			{
 				base = "0123456789abcdef";
 				ft_putnbr_base_fd((unsigned int)va_arg(args, unsigned int), base, 1);
 			}
-			if (format[i] == 'X')
+			if (format[i] == 'X')t
 			{
 				base = "0123456789ABCDEF";
 				ft_putnbr_base_fd((unsigned int)va_arg(args, unsigned int), base, 1);
@@ -88,7 +112,7 @@ int	ft_printf(const char *format, ...)
 			
 		}
 		else
-			write(1, &format[i], 1);
+			ft_putchar_fd(format[i], 1);
 		i++;
 	}
 	va_end(args);
@@ -102,8 +126,7 @@ int	main(void)
 
 	n = 42;
 	pointer = &n;
-	printf("hola\n");
-	ft_printf("ft_printf\nn1:%s n2:%d n3:%% n4:%c n5:%p n6:%u n7:%x n8:%X\n", NULL, 20, 'R', ((void *)0) , 2147483647, 500, 3500);
-	printf("printf\nn1:%s n2:%d n3:%% n4:%c n5:%p n6:%u n7:%x n8:%X\n", NULL, 20, 'R', ((void *)0), 2147483647, 500, 3500);
+	ft_printf("ft_printf\nn1:%s n2:%d n3:%% n4:%c n5:%p n6:%u n7:%x n8:%X\n", ((char *)0), 20, '\0', ((void *)0) , 2147483647, 3500, 3500);
+	printf("printf\nn1:%s n2:%d n3:%% n4:%c n5:%p n6:%u n7:%x n8:%X\n", (char *)0, 20, '\0', ((void *)0), 2147483647, 3500, 3500);
 	return (0);
 }
